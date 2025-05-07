d37782be177f7c13ac0def26308fca0321bc5659
email <beaprogrammer240gmail.com>
2025-05-07 18:04:36 +0500

    Initial commit

--- src/Restricted.jsx
+++ src/Restricted.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import GLBViewer from './GLBViewer';
-import padlock from '../public/padlock.png';
+// import padlock from '../public/padlock.png';
 import logoFull from '../public/logo-full.png';
 // import set1 from '../public/set1.glb';
 // import set2 from '../public/set2.glb';
@@ -43,7 +43,9 @@ const Restricted = () => {
                 <div className="w-full max-w-md p-8 space-y-8 bg-[#242424] border-[#3F3F3F] rounded-lg shadow-lg">
                     <div className="flex flex-col items-center justify-center">
                         <div className="relative mb-6">
-                            <img src={padlock} alt='lock' className='w-[175px] h-[175px]' />
+                            {/* <img src={padlock} alt='lock' className='w-[175px] h-[175px]' /> */}
+                            <img src="/padlock.png" alt="padlock" />
+
                         </div>
                         <h1 className="text-2xl font-bold text-center mb-8">YOU DO NOT HAVE THE XP</h1>
                         <form onSubmit={handleSubmit} className="w-full space-y-4">
@@ -70,18 +72,17 @@ const Restricted = () => {
             ) : (
                 <>
                     <div className="absolute inset-0 z-[-1] opacity-10 bg-contain bg-no-repeat bg-center" style={{ backgroundImage: `url('major-world.png')` }}></div>
-                    <div className="absolute inset-0 z-[-1] opacity-10 bg-contain bg-no-repeat bg-center" style={{ backgroundImage: `url('major-world.png')` }}></div>
+                    {/* <div className="absolute inset-0 z-[-1] opacity-10 bg-contain bg-no-repeat bg-center" style={{ backgroundImage: `url('major-world.png')` }}></div> */}
 
 
 
-                    <nav className=" shadow sticky top-0 bg-[#242424] opacity-90 w-full border-b border-[#3F3F3F]">
+                    <nav className="shadow-md sticky top-0 bg-white w-full border-b border-gray-200 z-50">
                         <div className="w-full px-4">
                             <div className="flex justify-between">
                                 <div className="flex space-x-4 justify-between w-full">
 
-
                                     <div className="hidden md:flex items-center justify-start space-x-1 w-1/3">
-                                        <a href="#" className="py-5 px-3 text-gray-200 font-medium hover:text-[#e63946] border-b-2 border-[#e63946] ">
+                                        <a href="#" className="py-5 px-3 text-black font-medium hover:text-[#e63946] border-b-2 border-[#e63946]">
                                             <MenuIcon width={24} height={24} fill="none" className="cursor-pointer hover:text-[#e63946]" stroke="currentColor" />
                                         </a>
                                         <div className="relative inline-block">
@@ -89,38 +90,35 @@ const Restricted = () => {
                                                 onMouseLeave={() => setIsCollectionHovered(false)} className="py-5 px-3 text-black font-bold hover:text-[#e63946]">Collections</a>
                                             {isCollectionHovered && (
                                                 <div className="absolute top-full left-0 mt-2 w-48 bg-white shadow-lg rounded-md z-50"
-
                                                     onMouseEnter={() => setIsCollectionHovered(true)}
                                                     onMouseLeave={() => setIsCollectionHovered(false)}
                                                 >
-                                                    <a href="#" className="block px-4 py-2 bg-[#242424] hover:text-[#e63946]">
+                                                    <a href="#" className="block px-4 py-2 text-black hover:text-[#e63946]">
                                                         Championship Icy Silver
                                                     </a>
-                                                    <a href="#" className="block px-4 py-2 bg-[#242424] hover:text-[#e63946]">
+                                                    <a href="#" className="block px-4 py-2 text-black hover:text-[#e63946]">
                                                         Championship Icy Blue
                                                     </a>
-                                                    <a href="#" className="block px-4 py-2 bg-[#242424] hover:text-[#e63946]">
+                                                    <a href="#" className="block px-4 py-2 text-black hover:text-[#e63946]">
                                                         Championship Royal Gold
                                                     </a>
                                                 </div>
                                             )}
                                         </div>
                                     </div>
-                                    <div className="w-1/1 flex justify-end">
+
+                                    <div className="w-full flex justify-start">
                                         <img src={logoFull} alt="major" className='w-[75px]' />
                                     </div>
-
-
                                     <div className="w-1/3 hidden md:flex items-center justify-end space-x-4">
-                                        <PowerIcon onClick={() => setIsAuthenticated(false)} width={24} height={24} fill="none" className="cursor-pointer hover:text-[#e63946]" stroke="currentColor" />
-                                        <ShopIcon width={24} height={24} fill="none" className="cursor-pointer hover:text-[#e63946]" stroke="currentColor" />
-                                        {/* <button onClick={() => setIsAuthenticated(false)} className="px-4 py-1 bg-[#E63946] hover:bg-[#DF3844] rounded-md transition-colors duration-300 cursor-pointer">Logout</button> */}
+                                        <PowerIcon onClick={() => setIsAuthenticated(false)} width={24} height={24} fill="none" className="cursor-pointer text-black hover:text-[#e63946]" stroke="currentColor" />
+                                        <ShopIcon width={24} height={24} fill="none" className="cursor-pointer text-black hover:text-[#e63946]" stroke="currentColor" />
                                     </div>
                                 </div>
 
                                 <div className="md:hidden flex items-center">
-                                    <button onClick={toggleMobileMenu} className="p-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-200">
-                                        <svg className="h-6 w-6 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <button onClick={toggleMobileMenu} className="p-2 rounded-md hover:bg-gray-200 transition duration-200">
+                                        <svg className="h-6 w-6 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                                         </svg>
                                     </button>
@@ -129,16 +127,12 @@ const Restricted = () => {
                         </div>
 
                         <div className={`mobile-menu md:hidden ${isMobileMenuOpen ? "" : "hidden"}`}>
-                            {/* <a href="#" className="block py-2 px-4 text-gray-800 dark:text-white bg-blue-50 dark:bg-blue-900 font-medium border-l-4 border-blue-500 dark:border-blue-400">Home</a>
-            <a href="#" className="block py-2 px-4 text-gray-800 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">Products</a>
-            <a href="#" className="block py-2 px-4 text-gray-800 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">Services</a>
-            <a href="#" className="block py-2 px-4 text-gray-800 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">About</a>
-            <a href="#" className="block py-2 px-4 text-gray-800 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">Contact</a> */}
-                            <div className="py-2 px-4 border-t border-gray-200 dark:border-gray-700">
-                                <button onClick={() => setIsAuthenticated(false)} className="block my-2 w-full px-4 py-2 bg-[#E63946] hover:bg-[#DF3844] rounded-md transition-colors duration-300 cursor-pointer">Logout</button>
+                            <div className="py-2 px-4 border-t border-gray-200">
+                                <button onClick={() => setIsAuthenticated(false)} className="block my-2 w-full px-4 py-2 bg-[#E63946] hover:bg-[#DF3844] rounded-md transition-colors duration-300 cursor-pointer text-white">Logout</button>
                             </div>
                         </div>
                     </nav>
+
 
                     {/* <div className='w-full'>
                         <header className="flex justify-center items-center mb-12 p-6">
@@ -147,7 +141,7 @@ const Restricted = () => {
                             </span>
                         </header>
                     </div> */}
-                    <div className="w-1/3 flex justify-end">
+                    <div className="w-full flex justify-start">
                         <img src={logoFull} alt="major" className='w-[75px]' />
                     </div>
 
